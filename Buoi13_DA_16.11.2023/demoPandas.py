bfbea7c33b6d82ebbaf593dc4ff6886f428310a3
vuduykhuong1412 <khuongvd.brainos@gmail.com>
2023-11-16 21:09:19 +0700

    Commit csv file

--- Buoi13_DA_16.11.2023/demoPandas.py
+++ Buoi13_DA_16.11.2023/demoPandas.py
@@ -28,66 +28,66 @@ import numpy as np
 
 #DATAFRAME
 
-table = {
-    'id': [1, 2, 3],
-    'price': [ 5000, 10000, 80000],
-    'quantity': [10, 15, 12]
-}
-df = pd.DataFrame(table, columns=['id', 'price', 'quantity'])
-print(df)
+# table = {
+#     'id': [1, 2, 3],
+#     'price': [ 5000, 10000, 80000],
+#     'quantity': [10, 15, 12]
+# }
+# df = pd.DataFrame(table, columns=['id', 'price', 'quantity'])
+# print(df)
 
-print(df['price'][0])
-print(df['quantity'][2])
+# print(df['price'][0])
+# print(df['quantity'][2])
 
-# print(df.loc[0])
-# print(df.loc[1])
-# print(df.loc[2])
+# # print(df.loc[0])
+# # print(df.loc[1])
+# # print(df.loc[2])
 
-print(df.loc[[0,1]])
-print(df.loc[[0,1]][['id', 'price']])
+# print(df.loc[[0,1]])
+# print(df.loc[[0,1]][['id', 'price']])
 
-# indexing
-print(df.index)
-print(df.columns)
+# # indexing
+# print(df.index)
+# print(df.columns)
 
-# create dataframe from series
-x = pd.Series([23, 45, 1, 55, 78])
-df2 = pd.DataFrame(x)
-print(df2)
+# # create dataframe from series
+# x = pd.Series([23, 45, 1, 55, 78])
+# df2 = pd.DataFrame(x)
+# print(df2)
 
-df3 = pd.DataFrame(x, columns=['age'])
-print(df3)
+# df3 = pd.DataFrame(x, columns=['age'])
+# print(df3)
 
-# Create dataframe from 2 dict
-data = [
-    {'a': "blue", 'b':"yellow", 'c': 'green'},
-    {'a': "Python", 'b': "C++", 'c':"Java"}
-]
+# # Create dataframe from 2 dict
+# data = [
+#     {'a': "blue", 'b':"yellow", 'c': 'green'},
+#     {'a': "Python", 'b': "C++", 'c':"Java"}
+# ]
 
-df4 = pd.DataFrame(data)
-print(df4)
+# df4 = pd.DataFrame(data)
+# print(df4)
 
-# create dataframe from Numpy
-numbers = [[123, 456],[789, 101112]]
-df5 = pd.DataFrame(numbers, columns=['Pandas', 'Numpy'])
-print(df5)
+# # create dataframe from Numpy
+# numbers = [[123, 456],[789, 101112]]
+# df5 = pd.DataFrame(numbers, columns=['Pandas', 'Numpy'])
+# print(df5)
 
-data = {'Country': ['Belgium', 'India', 'Brazil'], 
-   'Capital': ['Brussels', 'New Delhi', 'Brasília'], 
-   'Population': [11190846, 1303171035, 207847528]}
+# data = {'Country': ['Belgium', 'India', 'Brazil'], 
+#    'Capital': ['Brussels', 'New Delhi', 'Brasília'], 
+#    'Population': [11190846, 1303171035, 207847528]}
 
-df6 = pd.DataFrame(data, columns=['Country', 'Capital', 'Population'])
-# select by position
-print(df6.iloc[0,1])
-print(df6.iat[0,1])
+# df6 = pd.DataFrame(data, columns=['Country', 'Capital', 'Population'])
+# # select by position
+# print(df6.iloc[0,1])
+# print(df6.iat[0,1])
 
-# select by label
-print(df6.loc[0, 'Country'])
-print(df6.loc[0, 'Population'])
-print(df6.loc[0])
+# # select by label
+# print(df6.loc[0, 'Country'])
+# print(df6.loc[0, 'Population'])
+# print(df6.loc[0])
 
-print(df6.at[0, 'Country'])
-print(df6.at[0, 'Capital'])
+# print(df6.at[0, 'Country'])
+# print(df6.at[0, 'Capital'])
 
 
 
@@ -95,3 +95,12 @@ print(df6.at[0, 'Capital'])
 
 
 # Load file in Dataframe
+txt_path = 'GITHUB//VIETECH_DA_28.09.2023//Buoi13_DA_16.11.2023//data.txt'
+excel_path = 'GITHUB//VIETECH_DA_28.09.2023//Buoi13_DA_16.11.2023//demo.xlsx'
+
+df_txt = pd.read_csv(txt_path, sep=',')
+#print(df_txt)
+
+df_excel = pd.read_excel(excel_path)
+print(df_excel)
+print("Rowx Column :",  df_excel.shape)
