819ed2bf1a025c974063a74c29be58bf41ba28dd
duccuong220805 <cuongbibo2005@gmail.com>
2023-11-27 20:52:25 +0700

    message bai tap

---
+++ LOP_TANG_CUONG/Buoi04.22.11.2023/Đức Cường/hw4.py
@@ -0,0 +1,26 @@
+# Bai 4.1
+# aList = [1,2,3,4,5,6,7]
+# for i in range(0,7):
+#     aList[i] = int(aList[i])**2
+# print(aList)
+
+# Bài 4.2
+# list1 = ["M", "na", "i", "Peter"]
+# list2 = ["y", "me", "s", "Parker"]
+# list3 = [" ", " ", " ", " "]
+# for i in range(0,4):
+#     list3[i] = list1[i] + list2[i]
+# print(list3)
+
+# Bài 4.3
+# list1 = ["Hello ", "take "]
+# list2 = ["Dear", "Sir"]
+# list3 = []
+# for i in range(0,2):
+#     for u in range(0,2):
+#         list3.append(list1[i] + list2[u])
+# print(list3)
+
+
+
+
