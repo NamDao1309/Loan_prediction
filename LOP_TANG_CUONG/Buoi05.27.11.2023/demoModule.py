43a32cd24f52e9a0638ed41097a8fa910e98a25e
vuduykhuong1412 <khuongvd.brainos@gmail.com>
2023-11-27 20:39:09 +0700

    Update package

--- LOP_TANG_CUONG/Buoi05.27.11.2023/demoModule.py
+++ LOP_TANG_CUONG/Buoi05.27.11.2023/demoModule.py
@@ -1,12 +1,12 @@
 import moduleSum
-import moduleSub
-import moduleMulti
+from moduleSub import sub
+from moduleDiv import div as divi
 import moduleDiv
 
 if __name__ == "__main__":
     a = float(input("a = "))
     b = float(input("b = "))
     print("Tổng :", moduleSum.sum(a, b))
-    print("Hiệu :", moduleSub.sub(a, b))
-    print("Tích :", moduleMulti.multi(a, b))
+    print("Hiệu :", sub(a, b))
+    print("Tích :", divi.multi(a, b))
     print("Thương :", moduleDiv.div(a, b))
